312e71067d289411a91383be8cce608cc4626741
SonarBeserk <sonarbeserk@gmail.com>
2023-02-13 16:10:44 -0500

    Fix timemachine backups by adding excludes file and passing in parameters

--- install.sh
+++ install.sh
@@ -33,23 +33,32 @@ done
 curl -sS https://starship.rs/install.sh | sh
 
 while true; do
-    read -rp "Do you want to setup time machine https://github.com/cytopia/linux-timemachine backups? [Y/N]" yn
+    read -rp "Do you want to setup time machine https://github.com/cytopia/linux-timemachine backups? [Y/N] " yn
     case $yn in
         [Yy]* )
-            echo "Cloning time machine repo"
-            git clone https://github.com/cytopia/linux-timemachine.git ~/linux-timemachine
-            cd ~/linux-timemachine || echo "Failing to cd to ~/timemachine" && exit
+            if [[ ! -d ~/linux-timemachine ]];
+            then
+                echo "Cloning time machine repo"
+                git clone https://github.com/cytopia/linux-timemachine.git ~/linux-timemachine
+            fi
+            echo "Updating timemachine repo"
+            pushd ~/linux-timemachine > /dev/null || echo "Failing to cd to ~/timemachine"
             git checkout master && git pull
             echo "Building time machine binary"
             sudo make install
-            echo "Creating backups marker"
+            popd > /dev/null || echo "Failing to cd back from ~/timemachine"
+            echo "Creating Backups marker"
             touch ~/.check-backups
+            echo "Creating backup excludes file"
+            sed -i "s/\[\$USER\]/$USER/" "$(pwd)"/excludefrombackups.txt
+            ln -s "$(pwd)"/"excludefrombackups.txt" ~/excludefrombackups.txt
             echo "Please create the appropriate cron rule for backups similar to the following rule:"
-            echo "0 */8 * * * if [[ -e /timemachine ]]; then /usr/local/bin/timemachine ~ /timemachine; fi"
+            # This command runs the timemachine command every 8 hours while excluding the files in the exclude file and ignoring mountpoints to avoid recursive backups
+            echo "0 */8 * * * if [[ -e /timemachine ]]; then /usr/local/bin/timemachine ~ /timemachine -- --exclude-from excludefrombackups.txt --one-file-system; fi"
             break;
             ;;
 
-        [Nn]* ) break;;
-        * ) echo "Do you want to setup time machine https://github.com/cytopia/linux-timemachine backups?";;
+        [Nn]* ) echo "exit" && break;;
+        * ) echo "Do you want to setup time machine https://github.com/cytopia/linux-timemachine backups? [Y/N] ";;
     esac
 done
